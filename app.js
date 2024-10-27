01e25d33d87c6f863a29534394317ab02f333e8c
briskaanandaa <briskaananda103@gmail.com>
2024-10-27 12:08:31 +0700

    Menambahkan Fitur make-file

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const fs = require("node:fs")
-const readline = require('node:readline');
+const fs = require("node:fs");
+const readline = require("node:readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const app = {}
+const app = {};
 
 // contoh script pembuatan folder
- app.makeFolder = () => {
-    rl.question("Masukan Nama Folder : ",(folderName) => {
-        fs.mkdir(__dirname + `/${folderName}`,() => {
-            console.log("success created new folder");
-            
-        })
-        rl.close()
-    })
-} 
+app.makeFolder = () => {
+  rl.question("Masukan Nama Folder : ", (folderName) => {
+    fs.mkdir(__dirname + `/${folderName}`, () => {
+      console.log("Berhasil membuat folder baru");
+    });
+    rl.close();
+  });
+};
 
-// To Do : lanjutkan pembuatan logic disini 
+// Membuat File
+app.makeFile = () => {
+  rl.question(
+    "Masukkan Nama File (beserta ekstensinya contoh : file.js) : ",
+    (fileName) => {
+      fs.writeFile(path.join(__dirname, fileName), "", (err) => {
+        if (err) {
+          console.error("Kesalahan saat membuat file:", err);
+        } else {
+          console.log("Berhasil membuat file baru");
+        }
+        rl.close();
+      });
+    }
+  );
+};
 
-
-module.exports = app
+module.exports = app;
--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const args = process.argv.slice(2);
 const command = args[0];
 
 switch (command) {
-    case "make-folder":
-        app.makeFolder()
-        break;
-
-    default:
-        throw Error("Invalid command")
-        break;
+  case "make-folder":
+    app.makeFolder();
+    break;
+  case "make-file":
+    app.makeFile();
+    break;
+  default:
+    throw Error("Invalid command");
+    break;
 }
- 
-
